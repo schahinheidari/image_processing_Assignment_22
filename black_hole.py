83dabb8e9dc3ec74b0b2b9d500113a15523316c4
schahin0001 <schahinheidari@gmail.com>
2022-03-19 14:08:31 +0100

    update third assignment

--- black_hole.py
+++ black_hole.py
@@ -2,10 +2,9 @@ import cv2 as cv
 import numpy as np
 
 images = [[0 for i in range(5)] for j in range(4)]
-
 for i in range(4):
     for j in range(5):
-        images[i][j] = cv.imread(str(i + 1) + '/' + str(j + 1) + '.jpg')
+        images[i][j] = cv.imread(str(i + 1) + '/home/shahin/Desktop/Sajjad project/Assignment-22/image_processing_Assignment_22/images/black_hole' + str(j + 1) + '.jpg')
         images[i][j] = cv.cvtColor(images[i][j], cv.COLOR_BGR2GRAY)
 
 image_without_noise = [0 for i in range(4)]
@@ -16,13 +15,4 @@ for i in range(4):
 
 final_image = np.zeros((2000, 2000), dtype=np.uint8)
 
-final_image[0:1000, 0:1000] = image_without_noise[0]
-final_image[0:1000, 1000:2000] = image_without_noise[1]
-final_image[1000:2000, 0:1000] = image_without_noise[2]
-final_image[1000:2000, 1000:2000] = image_without_noise[3]
-
-cv.imshow('Final Image', final_image)
-
-cv.imwrite('final_image.jpg', final_image)
-
-cv.waitKey()
+print(image_without_noise)
